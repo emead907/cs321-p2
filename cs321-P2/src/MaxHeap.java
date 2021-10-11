a83edce74e4e4cd26ee28f6834ca8c8f5cae87f7
emead907 <42479682+emead907@users.noreply.github.com>
2021-10-11 13:58:14 -0600

    Update MaxHeap.java

--- cs321-P2/src/MaxHeap.java
+++ cs321-P2/src/MaxHeap.java
@@ -4,16 +4,16 @@ public class MaxHeap {
         //Heap insert puts a new element in the last spot of the array and call increae key on that index
     //increase key - look at pg 185
 
-    private int heapSize;
-    private Process A[];
+    protected int heapSize;
+    protected Process A[];
 
     public MaxHeap() {
         heapSize = 0;
         A = new Process[10];
     }
 
-    public boolean headIsEmpty(){
-        if(heapsize == 0){
+    public boolean heapIsEmpty(){
+        if(heapSize == 0){
             return true;
         }
         return false;
@@ -25,13 +25,13 @@ public class MaxHeap {
 
         int largest = -1;
 
-        if(left < heapSize && A[left].compareTo(A[index]) == 0) {
+        if(left < heapSize && A[left].compareTo(A[index]) == -1) {
             largest = left;
         }
         else {
             largest = index;
         }
-        if (right < heapSize && A[right].compareTo(A[largest]) == 1) {
+        if (right < heapSize && A[right].compareTo(A[largest]) == -1) {
             largest = right;
         }
 
@@ -51,39 +51,59 @@ public class MaxHeap {
 
     //return max, let it 0 call hepify return last one.
 
-    public Process extractMax(){
-        Process returnMe = A[0];
-
-        return returnMe;
-    }
-    private Process extraxtMax()
+    public Process extractMax() throws HeapException
     {
         if(heapSize < 1){
-            System.out.println("Cant extract max on a emptty heap");
+            throw new HeapException(("You cant extract from an empty heap"));
         }
         else {
             Process max = A[0];
-            A[0] = A[heapSize];
+            A[0] = A[heapSize - 1];
             heapSize--;
             MaxHeapify(0);
             return max;
         }
     }
 
-    public void increaseKey(int index, int prority){
-        if(prority < A[index]){
-            System.out.println("Key can only go up");
+    public void increaseKey(int index, int priority) throws HeapException
+    {
+        if(priority < A[index].getPriority()) {
+            throw new HeapException(("Key can only go up"));
         }
-
-
         else {
+            A[index].setPriority(priority);
 
+            while(index > 0 && A[Math.floorDiv((index -1), 2)].compareTo(A[index]) == 1) {
+                swap(index, Math.floorDiv((index -1), 2));
+                index = Math.floorDiv((index -1), 2);
+            }
         }
     }
 
-    public void incertProcess(Process p){
-
+    public void insertProcess(Process p){
+        if(heapSize >= A.length){ //Need to expand the array
+            expandArray(A);
+        }
+        int pPri = p.getPriority();
+        A[heapSize] = p;
+        A[heapSize].setPriority(Integer.MIN_VALUE);
+        heapSize++;
+        try {
+            increaseKey(heapSize - 1, pPri);
+        } catch (HeapException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
+
+    private void expandArray(Process[] oldArray){
+        Process[] newArray = new Process[oldArray.length * 2];
+        for(int i = 0; i < oldArray.length; i++) {
+            newArray[i] = oldArray[i];
+        }
+        A = newArray;
+    }
+
 }
 
 
