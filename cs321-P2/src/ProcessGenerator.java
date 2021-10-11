fd20a7ea25c0f84491944706fa15e0a65e78111f
emead907 <42479682+emead907@users.noreply.github.com>
2021-10-11 13:59:31 -0600

    Update ProcessGenerator.java

--- cs321-P2/src/ProcessGenerator.java
+++ cs321-P2/src/ProcessGenerator.java
@@ -1,21 +1,32 @@
+import java.util.Random;
 public class ProcessGenerator implements ProcessGeneratorInterface{
 
-    public ProcessGenerator(double probability, long seed){
+    private Random random;
+    private double prob;
 
+    public ProcessGenerator(double probability, long seed){
+        random = new Random(seed);
+        prob = probability;
     }
 
     public ProcessGenerator(double probability){
-
+        random = new Random();
+        prob = probability;
     }
 
     @Override
     public Process getNewProcess(int currentTime, int maxProcessTime, int maxPriority) {
-        Process newJob = new Process(currentTime);
+        int pri = random.nextInt(maxPriority) + 1;
+        int time = random.nextInt(maxProcessTime) + 1;
+        Process newJob = new Process(currentTime, pri, time);
         return newJob;
     }
 
     @Override
     public boolean query() {
+        if(random.nextDouble() < prob){
+            return true;
+        }
         return false;
     }
 }
